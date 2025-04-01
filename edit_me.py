76dfb39cd8f5a0f70dc788c556d7864914b33dee
Tevin Borders <tborders002@drury.edu>
2025-03-31 20:25:18 -0500

    Lab 1 Finished

--- edit_me.py
+++ edit_me.py
@@ -4,10 +4,9 @@ def find_max(lst):
 
     current_max = lst[0]
 
-    #
-    #  YOUR CODE GOES HERE
-    #
-
+    for i in range(len(lst)):
+        if (lst[i] > current_max):
+            current_max = lst[i]
     return current_max
 
 test_list = [2112*i % 2024 for i in range(10000)]
